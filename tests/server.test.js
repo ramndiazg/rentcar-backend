5b9c2798a2cc5b3f736454ff2bdb9fd7a5743342
Ramon Diaz <r.diaz@intellisys.com.do>
2024-11-02 16:31:03 -0400

    fixing test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('../server'); // Asegúrate de que esta ruta sea correcta
 
-describe('GET /', () => {
+describe('GET /api', () => {
   it('should return a 200 status code', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get('/api');
     expect(res.statusCode).toBe(200);
   });
 });
